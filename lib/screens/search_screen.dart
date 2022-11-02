b2164599e08d4e1cfba57f3a1b2d8330d724c315
garrenc <vetoshev.ruslan@gmail.com>
2022-11-02 20:48:28 +0100

    search screen improvement

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -17,6 +17,8 @@ class _SearchScreenState extends State<SearchScreen> {
   List<Product> products = [];
   List<Product> allProducts = [];
   ProductsBloc bloc = ProductsBloc();
+  String input = '';
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
@@ -41,40 +43,54 @@ class _SearchScreenState extends State<SearchScreen> {
             title: const Text('Поиск'),
             backgroundColor: Colors.white,
           ),
-          body: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(16),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search),
-                    hintText: 'Введите название товара',
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20), borderSide: const BorderSide(color: Colors.black)),
-                  ),
-                  onChanged: searchProduct,
-                ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemBuilder: (ctx, index) {
-                    var product = products[index];
+          body: state is ProductsLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(16),
+                      child: Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            prefixIcon: const Icon(Icons.search),
+                            suffixIcon: controller.text.isNotEmpty
+                                ? IconButton(
+                                    onPressed: () => setState(() {
+                                          controller.text = '';
+                                          products = allProducts;
+                                        }),
+                                    icon: const Icon(Icons.clear))
+                                : null,
+                            hintText: 'Введите название товара',
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(100), borderSide: const BorderSide(color: Colors.black)),
+                          ),
+                          onChanged: searchProduct,
+                          controller: controller,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (ctx, index) {
+                          var product = products[index];
 
-                    return Column(
-                      children: [
-                        ProductItem(
-                          product: product,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                      ],
-                    );
-                  },
-                  itemCount: products.length,
+                          return Column(
+                            children: [
+                              ProductItem(
+                                product: product,
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                            ],
+                          );
+                        },
+                        itemCount: products.length,
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
         );
       },
     );
@@ -83,9 +99,8 @@ class _SearchScreenState extends State<SearchScreen> {
   void searchProduct(String query) {
     final suggestions = allProducts.where((product) {
       final productTitle = product.title.toLowerCase();
-      final input = query.toLowerCase();
 
-      return productTitle.contains(input);
+      return productTitle.contains(controller.text.toLowerCase());
     }).toList();
 
     setState(() => products = suggestions);
